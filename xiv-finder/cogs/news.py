695549f896c8daceedc399f100d3c9b2743337cd
haydenfish <hayden.fish@icloud.com>
2023-12-13 21:11:42 -0800

    News topic command response is now formatted and sent as embed msg, and additionally added validation for topic month 12.13.2023

--- xiv-finder/cogs/news.py
+++ xiv-finder/cogs/news.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 import requests
 
 """
@@ -15,14 +16,16 @@ class News(commands.Cog):
         self.bot = bot
 
     """
-    Sends list of this months news topics to user
+    Sends list of embed messages containing data relating to this months news topics to user,
+    length of list is defined by optional command arg, default value is 10
     """
     @commands.command()
-    async def topics(self, ctx):
+    async def topics(self, ctx, result_limit = 10):
         topic_list = self.request_topic_list()
 
-        for topic in topic_list:
-            await ctx.send(topic['title'])
+        for topic in topic_list[:int(result_limit)]:
+            if self.is_same_month(topic):
+                await ctx.send(embed = self.create_news_embed(topic))
     
     def request_topic_list(self):
         search_url = URL + "/topics"
@@ -33,13 +36,24 @@ class News(commands.Cog):
 
         return load_response
     
-    # TODO: return embed object containing topic related data
-    def news_embed(self):
-        return
+    def create_news_embed(self, topic):
+        if topic is None:
+            return 
+
+        news = discord.Embed(title = topic['title'], url = topic['url'], color = discord.Color.red())
+
+        news.add_field(name = 'Description', value = topic['description'])
+        news.set_image(url = topic['image'])
+        news.set_footer(text = topic['time'])
+
+        return news
     
-    # TODO: create method to verify topic is from the current month
-    def verify_time(self):
-        return
+    def is_same_month(self, topic):
+        current_month = datetime.now().strftime('%m').replace('0', '')
+
+        topic_month = str(topic['time']).split('-')[1]
+
+        return current_month == topic_month
 
 async def setup(bot):
     await bot.add_cog(News(bot))
